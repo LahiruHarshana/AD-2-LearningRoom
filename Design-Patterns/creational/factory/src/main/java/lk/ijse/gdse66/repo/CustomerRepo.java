d3a5d59e5fe4610e931ac8cfbbb6fcca607e33ea
LahiruHarshana <lharshana2002@gmail.com>
2024-05-09 10:12:02 +0530

    add facade desighn pattern

--- Design-Patterns/creational/factory/src/main/java/lk/ijse/gdse66/repo/CustomerRepo.java
+++ Design-Patterns/creational/factory/src/main/java/lk/ijse/gdse66/repo/CustomerRepo.java
@@ -1,11 +1,13 @@
 package lk.ijse.gdse66.repo;
 
-/**
+import lk.ijse.gdse66.repo.impl.StudentRepoImpl; /**
  * @author : L.H.J
  * @File: CustomerRepo
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-05-09, Thursday
  **/
+
+
 public interface CustomerRepo extends SuperRepo {
 
 }
