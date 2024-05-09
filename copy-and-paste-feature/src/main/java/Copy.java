019a2e7cd427e5fa15f0cb4487bfc0f610a32f96
LahiruHarshana <lharshana2002@gmail.com>
2024-05-09 12:51:45 +0530

    adding

---
+++ copy-and-paste-feature/src/main/java/Copy.java
@@ -0,0 +1,67 @@
+/**
+ * @author : L.H.J
+ * @File: Copy
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-05-09, Thursday
+ **/
+public class Copy {
+
+    static String name = "Harshana";
+    static int age = 25;
+    static String address = "Colombo";
+    static String country = "Sri Lanka";
+    static String email = "lharshana2032gmail.com";
+    static String mobile = "0771234567";
+    public static void main(String[] args) {
+
+
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        Copy.name = name;
+    }
+
+    public static int getAge() {
+        return age;
+    }
+
+    public static void setAge(int age) {
+        Copy.age = age;
+    }
+
+    public static String getAddress() {
+        return address;
+    }
+
+    public static void setAddress(String address) {
+        Copy.address = address;
+    }
+
+    public static String getCountry() {
+        return country;
+    }
+
+    public static void setCountry(String country) {
+        Copy.country = country;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static void setEmail(String email) {
+        Copy.email = email;
+    }
+
+    public static String getMobile() {
+        return mobile;
+    }
+
+    public static void setMobile(String mobile) {
+        Copy.mobile = mobile;
+    }
+}
---
+++ copy-and-paste-feature/src/main/java/Paste.java
@@ -0,0 +1,24 @@
+/**
+ * @author : L.H.J
+ * @File: Paste
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-05-09, Thursday
+ **/
+public class Paste {
+
+        static String name = "Harshana";
+        static int age = 25;
+        static String address = "Colombo";
+        static String country = "Sri Lanka";
+        static String email = "lharshana2032gmail.com";
+        static String mobile = "0771234567";
+        public static void main(String[] args) {
+
+            System.out.println("Name : " + name);
+            System.out.println("Age : " + age);
+            System.out.println("Address : " + address);
+            System.out.println("Country : " + country);
+            System.out.println("Email : " + email);
+            System.out.println("Mobile : " + mobile);
+        }
+}
