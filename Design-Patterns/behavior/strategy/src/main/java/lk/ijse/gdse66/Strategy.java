c543d090c2a5a6571081a1542dd4176b02e34cb7
LahiruHarshana <lharshana2002@gmail.com>
2024-05-09 11:35:45 +0530

    add strategy  desighn pattern

---
+++ Design-Patterns/behavior/strategy/src/main/java/lk/ijse/gdse66/Strategy.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse66;
+
+/**
+ * @author : L.H.J
+ * @File: Strategy
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-05-09, Thursday
+ **/
+public interface Strategy {
+}
