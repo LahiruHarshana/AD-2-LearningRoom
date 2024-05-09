be35ab336c6a82a53ea2bcd5ae8a0b14f69c03cc
LahiruHarshana <lharshana2002@gmail.com>
2024-05-09 10:14:42 +0530

    add facade desighn pattern

---
+++ Design-Patterns/structural/facade/src/main/java/lk/ijse/gdse66/Rectangle.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse66;
+
+/**
+ * @author : L.H.J
+ * @File: Rectangle
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-05-09, Thursday
+ **/
+public class Rectangle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+
+
+}
