0cee5007e37bb1e0609a910d672d38d663a44c05
LahiruHarshana <lharshana2002@gmail.com>
2024-05-09 10:15:17 +0530

    add facade desighn pattern

---
+++ Design-Patterns/structural/facade/src/main/java/lk/ijse/gdse66/Circle.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse66;
+
+/**
+ * @author : L.H.J
+ * @File: Circle
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-05-09, Thursday
+ **/
+public class Circle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
