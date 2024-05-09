590b3401c029ae676970cc246154976c3fe6b95e
LahiruHarshana <lharshana2002@gmail.com>
2024-05-09 11:11:58 +0530

    add decorator desighn pattern

---
+++ Design-Patterns/structural/decorator/src/main/java/lk/ijse/gdse66/RedShapeDecorator.java
@@ -0,0 +1,23 @@
+package lk.ijse.gdse66;
+
+/**
+ * @author : L.H.J
+ * @File: BasicShape
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-05-09, Thursday
+ **/
+public class RedShapeDecorator extends ShapeDecorator{
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+    }
+
+    private void setRedBorder(Shape decoratedShape){
+        System.out.println("Border Color: Red");
+    }
+}
