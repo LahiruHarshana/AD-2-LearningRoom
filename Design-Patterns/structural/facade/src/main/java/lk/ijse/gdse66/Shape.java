afe3c8b89ccbb169290b99bc8e64d035195269b6
LahiruHarshana <lharshana2002@gmail.com>
2024-05-09 10:12:41 +0530

    add facade desighn pattern

---
+++ Design-Patterns/structural/facade/src/main/java/lk/ijse/gdse66/Shape.java
@@ -0,0 +1,11 @@
+package lk.ijse.gdse66;
+
+/**
+ * @author : L.H.J
+ * @File: Shape
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-05-09, Thursday
+ **/
+public interface Shape {
+    void draw();
+}
